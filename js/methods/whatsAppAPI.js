0e651bf2bfd929c8a71d860e5062352ada7fa86a
Alexandre <alexandre.trieste@gmail.com>
2024-04-20 00:35:43 -0300

    Fix: error in layout

--- js/methods/whatsAppAPI.js
+++ js/methods/whatsAppAPI.js
@@ -1,8 +1,18 @@
-export let waAPI = {
+/*export let waAPI = {
     countryCode: "process.env.countryCode",
     number: "process.env.number",
     go: function(text) {
       window.open(`https://api.whatsapp.com/send?phone=${this.countryCode}${this.number}&text=${window.encodeURIComponent(text)}`);
     }
   }
-  
+  */
+
+
+export let waAPI = {
+    countryCode: "process_params.env.countryCode", // Acesse a variável de ambiente diretamente
+    number: "process_params.env.number", // Acesse a variável de ambiente diretamente
+    go: function(text) {
+      window.open(`https://api.whatsapp.com/send?phone=${this.countryCode}${this.number}&text=${window.encodeURIComponent(text)}`);
+    }
+};
+
