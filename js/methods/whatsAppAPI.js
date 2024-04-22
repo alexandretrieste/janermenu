e79e19bae9ac32cabd84bee3fafdb4391299209f
Alexandre <alexandre.trieste@gmail.com>
2024-04-22 06:36:28 -0300

    Fix: some error in wpp

--- js/methods/whatsAppAPI.js
+++ js/methods/whatsAppAPI.js
@@ -1,6 +1,6 @@
 export let waAPI = {
-    countryCode: process.env.countryCode,
-    number: process.env.number,
+    countryCode: "process.env.countryCode",
+    number: "process.env.number",
     go: function(text) {
       window.open(`https://api.whatsapp.com/send?phone=${this.countryCode}${this.number}&text=${window.encodeURIComponent(text)}`);
     }
